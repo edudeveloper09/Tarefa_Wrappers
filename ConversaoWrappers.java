1019c6e72d50b8e09d1ebd19daf850e06e2937c8
edudeveloper09 <157771672+edudeveloper09@users.noreply.github.com>
2024-04-05 14:54:25 -0300

    Add files via upload

---
+++ ConversaoWrappers.java
@@ -0,0 +1,14 @@
+public class ConversaoWrappers {
+
+    public static void main(String args[]) {
+
+        //Conversão de primitivo para Wrappers
+        Boolean status = false;
+        Boolean status1 = Boolean.FALSE;
+        Byte b = 8;
+        Float f = 2.0f;
+
+        System.out.println(b);
+        System.out.println(f);
+    }
+}
